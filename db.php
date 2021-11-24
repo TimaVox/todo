381c65c66307cb628bcffb1c9c64a73875ac44d7
Tima Vox <zeynalov.teimur@gmail.com>
2021-11-24 21:50:11 +0600

    Обновление todo #2

--- db.php
+++
@@ -1,9 +0,0 @@
-<?php
-return [
-    'type' => 'mysql',
-    'host' => 'localhost',
-    'dbname' => 'timavox',
-    'charset' => 'utf8',
-    'user' => 'timavox',
-    'pass' => '4h@f2MjPP@ADD4T'
-];
